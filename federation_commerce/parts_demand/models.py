078768113a4499ece14c566dbead2d8cc5e3fcc2
jcamachado <jose.machado@finxi.com.br>
2019-10-02 21:47:28 -0300

    feat: For now, the default state for Status is "Não Finalizado"

--- federation_commerce/parts_demand/models.py
+++ federation_commerce/parts_demand/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class PartsDemand(models.Model):
-    STATUS = (('YES', 'Finalizado'), ('NO', 'Não Finalizado'))
+    STATUS = ( ('Não Finalizado', 'Não Finalizado'), ('Finalizado', 'Finalizado'))
     description = models.CharField(max_length=500, blank=True, default='')
     delivery_addr = models.CharField(max_length=200, blank=True, default='')
     contact_info = models.TextField()
