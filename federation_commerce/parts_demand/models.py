d491d3875cf5a5c43f231ec0aec204b92b69a52b
jcamachado <jose.machado@finxi.com.br>
2019-10-02 21:14:38 -0300

    feat: Now, the status is togglable between "Finalizado" and "Não Finalizado"

--- federation_commerce/parts_demand/models.py
+++ federation_commerce/parts_demand/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 
 class PartsDemand(models.Model):
+    STATUS = (('YES', 'Finalizado'), ('NO', 'Não Finalizado'))
     description = models.CharField(max_length=500, blank=True, default='')
     delivery_addr = models.CharField(max_length=200, blank=True, default='')
     contact_info = models.TextField()
     owner = models.ForeignKey('auth.User', related_name='parts_demand', on_delete=models.CASCADE)
-    status = models.TextField(default="Não finalizado")
+    status = models.CharField(max_length=500, blank=False, choices=STATUS)
 
     def save(self, *args, **kwargs):
         super(PartsDemand, self).save(*args, **kwargs)
