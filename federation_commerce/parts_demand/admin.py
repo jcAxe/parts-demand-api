9fc0b0681cffbdf090e37daed78f15b27b6ef638
jcamachado <jose.machado@finxi.com.br>
2019-10-03 00:59:11 -0300

    feat: added PartsDemands model into the admin page

--- federation_commerce/parts_demand/admin.py
+++ federation_commerce/parts_demand/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import PartsDemand
+from django.utils.safestring import mark_safe
+from django.conf import settings
+import os
 
-# Register your models here.
+
+@admin.register(PartsDemand)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'description', 'delivery_addr', 'contact_info', 'owner', 'conclusion_status')
+    fields = ('conclusion_status', 'owner')
+
