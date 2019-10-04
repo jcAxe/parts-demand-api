cbfb5ed6b43ac55a07d3179661299b1f7c5d2e2a
Jose Machado <jcdealmeidam@gmail.com>
2019-10-04 16:18:07 -0300

    feat: added endpoint to close demand status

--- federation_commerce/parts_demand/urls.py
+++ federation_commerce/parts_demand/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('demands/', views.PartsDemandList.as_view()),
     path('demands/<int:pk>/', views.PartsDemandDetail.as_view()),
+    path('demands/<int:pk>/close/', views.CloseDemand.as_view()),
     path('users/', views.UserList.as_view()),
     path('users/<int:pk>/', views.UserDetail.as_view()),
 ]
--- federation_commerce/parts_demand/views.py
+++ federation_commerce/parts_demand/views.py
@@ -5,6 +5,7 @@ from .serializers import PartsDemandSerializer, UserSerializer
 from rest_framework import generics
 from django.contrib.auth.models import User
 from rest_framework import permissions
+from rest_framework.response import Response
 
 
 class PartsDemandList(generics.ListCreateAPIView):
@@ -34,6 +35,28 @@ class PartsDemandDetail(generics.RetrieveUpdateDestroyAPIView):
         elif self.request.user.username == "Administrador":
             return PartsDemand.objects.all()
         return PartsDemand.objects.filter(owner=self.request.user)
+
+
+class CloseDemand(generics.RetrieveAPIView):
+    serializer_class = PartsDemandSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        queryset = []
+        if self.request.user.is_anonymous:
+            return queryset
+        elif self.request.user.username == "Administrador":
+            return PartsDemand.objects.all()
+        return PartsDemand.objects.filter(owner=self.request.user)
+
+    def get_object(self):
+        object = super(CloseDemand, self).get_object()
+        object.conclusion_status = True
+        object.save()
+        return object
+
+
+
 
 
 class UserList(generics.ListAPIView):
