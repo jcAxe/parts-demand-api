21250856223e300227f00a36a7ebb5c784da5095
jcamachado <jose.machado@finxi.com.br>
2019-10-05 14:12:10 -0300

    feat: default admin boolean icons for true or false were replaced by the black ones

--- federation_commerce/parts_demand/admin.py
+++ federation_commerce/parts_demand/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
+from django.contrib.staticfiles.templatetags.staticfiles import static
+from django.utils.html import format_html
+
 from .models import PartsDemand
+import django.contrib.admin.templatetags.admin_list
 from django.utils.safestring import mark_safe
 from django.conf import settings
 import os
@@ -10,3 +14,10 @@ class AuthorAdmin(admin.ModelAdmin):
     list_display = ('id', 'description', 'delivery_addr', 'contact_info', 'owner', 'conclusion_status')
     fields = ('conclusion_status', 'owner')
 
+
+def _my_boolean_icon(field_val):
+    icon_url = static('imgs/%s.svg' % {True: 'yes', False: 'no', None: 'unknown'}[field_val])
+    return format_html('<img src="{}" alt="{}">', icon_url, field_val)
+
+
+django.contrib.admin.templatetags.admin_list._boolean_icon = _my_boolean_icon
